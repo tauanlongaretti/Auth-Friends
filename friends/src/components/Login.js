5d03a1a35ce2e2b94472fff9962f1df9a0d13e3a
tauan-longaretti <tauanlongaretti@gmail.com>
2020-02-15 00:03:42 -0500

    created event handlers and authentication for log in

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,21 +1,64 @@
-import React, { useState } from "react";
+import React from "react";
+import { axiosWithAuth } from "./utils/axiosWithAuth";
 
-const Login = props => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  return (
-    <div>
-      <label>
-        Username:
-        <input type="text"></input>
-      </label>
-      <label>
-        Password:
-        <input type="text"></input>
-      </label>
-      <button type="submit">Login</button>
-    </div>
-  );
-};
+class Login extends React.Component {
+  state = {
+    credentials: {
+      username: "",
+      password: ""
+    }
+  };
+
+  handleChange = e => {
+    this.setState({
+        credentials: {
+          ...this.state.credentials,
+          [e.target.name]: e.target.value
+        }
+      });
+  };
+
+  login = e => {
+      e.preventDefault();
+      axiosWithAuth()
+        .post("/login", this.state.credentials)
+        .then(res => {
+            localStorage.setItem("token", res.data.payload);
+            this.props.history.push("/protected");
+        })
+        .catch(err => {
+            localStorage.removeItem("token");
+            console.log("invalid login: ", err);
+        });
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.login}>
+          <label>
+            Username:
+            <input
+              type="text"
+              name="username"
+              value={this.state.credentials.username}
+              onChange={this.handleChange}
+            ></input>
+          </label>
+          <label>
+            Password:
+            <input
+              type="text"
+              name="password"
+              value={this.state.credentials.password}
+              onChange={this.handleChange}
+            ></input>
+          </label>
+          <button>Log in</button>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default Login;
