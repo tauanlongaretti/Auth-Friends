dda732184b44ee8dbe1a32eadf1c5c22ff2f5d1f
tauan-longaretti <tauanlongaretti@gmail.com>
2020-02-18 18:31:57 -0500

    PrivateRoute is in place and checking for token

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
-import './App.css';
+import "./App.css";
 import Login from "./components/Login";
+import ProctectedRoute from "./components/ProtectedRoute";
+import FriendsList from "./components/FriendsList";
 
 function App() {
   return (
     <Router>
+      <div className="links">
+      <Link className="link" to="/login">Login</Link>
+      <Link className="link" to="/protected">Friends List</Link>
+      </div>
       <div className="App">
-        <Route component={Login} />
+        <Switch>
+          <ProctectedRoute exact path="/protected" component={FriendsList} />
+          <Route path="/login" component={Login} />
+          <Route component={Login} />
+        </Switch>
       </div>
     </Router>
   );
